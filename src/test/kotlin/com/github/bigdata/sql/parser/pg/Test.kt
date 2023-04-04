369bf2ee190a0e02a5e1ccd63842eaca1ccd6d3e
duhanmin <hanmin.du@longbridge.sg>
2023-04-04 11:26:18 +0800

    ALIAS

--- src/test/kotlin/com/github/bigdata/sql/parser/pg/Test.kt
+++ src/test/kotlin/com/github/bigdata/sql/parser/pg/Test.kt
@@ -8,11 +8,7 @@ class Test {
     @Test
     fun setConfigTest1() {
         println("---------------")
-        var querySql =  "select id,created_at,updated_at,counter_id,trade_day ,open,high,low,close,turnover,volume,suspension_indicator" +
-                " from quote_eod_private where trade_day = to_char(current_date - interval '1 day','YYYY-MM-DD') " +
-                "and CURRENT_DATE > date(trade_day) " +
-                "union select id,created_at,updated_at,counter_id,trade_day,open," +
-                "high,low,close,turnover,volume,suspension_indicator from (select *,rank() over(partition by counter_id order by trade_day desc) as rownum from quote_eod_private where CURRENT_DATE > date(trade_day))t where rownum=1"
+        var querySql = "select id,counter_id,alias,former_name,former_ticker from security_extend"
         var statement = PostgreSQLHelper().parseSqlFieldLineage(querySql).getStatementData().statement
         println(JSONUtil.parseObj(statement))
     }
